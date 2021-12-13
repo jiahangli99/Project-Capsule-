07b71c427a7dd55cb3c71bb14e25f644c1d269b9
Abraham Li <liabraham.1999@gmail.com>
2021-12-13 11:07:11 -0700

    added on click function to draw 2 random cards and alert the other player to go next

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,21 @@
 $(function () {
     const deckUrl = 'https://deckofcardsapi.com/api/deck/new/draw/?count=2';
     const $form = $('form');
+    alert('Player 1 Please Draw 2 Cards');
 
+    $form.on('click', '.buttonred', handleGetDataOne);
+    function handleGetDataOne(a) {
+        a.preventDefault()
+        $.ajax(deckUrl).then(function (dataOne) {
+            renderOne(dataOne)
+            alert('Player 2 Please Draw 2 Cards');
+        })
+    };
+    function renderOne(gamerStats) {
+        $('.maintwo').html(`
+        <span class="one"><img src=${gamerStats.cards[0].image}><img src=${gamerStats.cards[1].image}></span>
+        `)
+    }
 
 
 
