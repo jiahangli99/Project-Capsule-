0d85e70f84531824da4edac7760d9e77ac3d3c52
Abraham Li <liabraham.1999@gmail.com>
2021-12-14 07:32:12 -0700

    added blue deck on click function

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,11 +1,15 @@
 $(function () {
+    //Created two variables
     const deckUrl = 'https://deckofcardsapi.com/api/deck/new/draw/?count=2';
     const $form = $('form');
+    //alerts the first player to press the button
     alert('Player 1 Please Draw 2 Cards');
-
+// red deck on click function
     $form.on('click', '.buttonred', handleGetDataOne);
     function handleGetDataOne(a) {
+        //prevent default so it doesnt reload the page on click
         a.preventDefault()
+        //loads the website then runs the function renderOne
         $.ajax(deckUrl).then(function (dataOne) {
             renderOne(dataOne)
             alert('Player 2 Please Draw 2 Cards');
@@ -15,6 +19,18 @@ $(function () {
         $('.mainone').html(`
         <span class="one"><img src=${gamerStats.cards[0].image}><img src=${gamerStats.cards[1].image}></span>
         `)
+    }
+    $form.on('click', '.buttonblue', handleGetDataTwo);
+    function handleGetDataTwo(a) {
+        a.preventDefault()
+        $.ajax(deckUrl).then(function (dataTwo) {
+            renderTwo(dataTwo)
+        })
+    }
+    function renderTwo(gamerStats) {
+        $('.maintwo').html(`
+    <span class="two"><img src=${gamerStats.cards[0].image}><img src=${gamerStats.cards[1].image}></span>
+    `)
     }
 
 
